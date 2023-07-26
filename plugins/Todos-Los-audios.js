29126c85fc66ccab0bc0f00985e53818a357f6e4
Whatsapp1231234 <91056671+Whatsapp1231234@users.noreply.github.com>
2023-07-26 14:05:57 +0300

    дгшнп

--- plugins/Todos-Los-audios.js
+++ plugins/Todos-Los-audios.js
@@ -72,8 +72,8 @@ let vn = './Audios/хочу.mp3'
 this.sendPresenceUpdate('recording', m.chat)   
 this.sendFile(m.chat, vn, 'error.mp3', null, m, true, {type: 'audioMessage', ptt: true})}
     
-if (!chat.isBanned && chat.audios && m.text.match(/(Fiesta del admin|fiesta del admin)/gi)) {    
-let vn = './Audios/admin.mp3'
+if (!chat.isBanned && chat.audios && m.text.match(/(спам|Спам)/gi)) {    
+let vn = './Audios/спам.mp3'
 this.sendPresenceUpdate('recording', m.chat)   
 this.sendFile(m.chat, vn, 'error.mp3', null, m, true, {type: 'audioMessage', ptt: true})}
     
